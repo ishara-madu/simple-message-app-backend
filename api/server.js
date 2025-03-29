67c8494c37ee09ea2e0dcdd0f77699398c9d3381
Ishara <isharamadushankab@gmail.com>
2025-03-29 15:11:24 +0530

    update 1 file and delete 1 file

--- api/server.js
+++ api/server.js
@@ -1,25 +1,31 @@
 import express from 'express';
-import { connectDB } from './config/db.js';
-import dotenv from 'dotenv';
-import messageRoute from './routes/message.route.js';
-import cors from 'cors';
-
-dotenv.config();
+import { connectDB } from '../config/db.js';
+import dotenv from 'dotenv'
+import messageRoute from '../routes/message.route.js'
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors'
+dotenv.config()
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST'],
+    },
+});
+app.use(cors());
+app.use(express.json())
+app.set('io', io);
+const PORT = 5000;
 
-// Middleware
-app.use(cors({
-    origin: 'https://simple-message-app-backend.vercel.app', // Adjust if frontend URL differs
-    methods: ['GET', 'POST'],
-}));
-app.use(express.json());
 
-// Routes
-app.use('/api/messages', messageRoute);
 
-// Connect to MongoDB
-connectDB();
+app.use('/api/messages', messageRoute)
 
-// Export for Vercel serverless
-export default app;
+
+server.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
